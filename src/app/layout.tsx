50f508f02e2d581afa54a2c638b87a9c6217a4f0
Rasik <vlad.fokin2004.vf@gmail.com>
2024-08-28 16:51:16 +0300

    Add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SessionWrapper from "@/components/providers/session-wrapper";
+import {Toaster} from "@/components/ui/toaster";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Cairos — Додаток для саморозвитку",
@@ -15,7 +17,12 @@ export default function RootLayout({
   return (
       <SessionWrapper>
         <html lang="en">
-            <body>{children}</body>
+            <body>
+            <main>
+                {children}
+                <Toaster />
+            </main>
+            </body>
         </html>
       </SessionWrapper>
   );
