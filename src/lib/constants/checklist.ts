92b57e876b5e34a306b7e750faa3bb198248feba
Rasik <vlad.fokin2004.vf@gmail.com>
2024-09-08 10:45:43 +0300

    Add temp checklist

---
+++ src/lib/constants/checklist.ts
@@ -0,0 +1,381 @@
+import {TChecks} from "@/lib/types";
+
+export const staticItems: TChecks[] = [
+    {
+        label: "Plan Review",
+        description: "Огляд та аналіз запланованих завдань."
+    },
+    {
+        label: "Resource Allocation",
+        description: "Розподіл ресурсів для виконання задач."
+    },
+    {
+        label: "Progress Tracking",
+        description: "Відстеження прогресу по задачах."
+    },
+    {
+        label: "Issue Resolution",
+        description: "Розв'язання проблем та усунення перешкод."
+    },
+    {
+        label: "Team Communication",
+        description: "Організація комунікації в команді для ефективної співпраці."
+    },
+    {
+        label: "Final Review",
+        description: "Підсумковий огляд і оцінка результатів."
+    }
+];
+
+export const cairosDaysTasks: Record<number, TChecks[]> = {
+    1: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    2: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    3: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    4: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    5: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    6: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+    0: [
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        },
+        {
+            label: "Plan Review",
+            description: "Огляд та аналіз запланованих завдань."
+        },
+        {
+            label: "Resource Allocation",
+            description: "Розподіл ресурсів для виконання задач."
+        },
+        {
+            label: "Progress Tracking",
+            description: "Відстеження прогресу по задачах."
+        },
+        {
+            label: "Issue Resolution",
+            description: "Розв'язання проблем та усунення перешкод."
+        },
+        {
+            label: "Team Communication",
+            description: "Організація комунікації в команді для ефективної співпраці."
+        },
+        {
+            label: "Final Review",
+            description: "Підсумковий огляд і оцінка результатів."
+        }
+    ],
+};
