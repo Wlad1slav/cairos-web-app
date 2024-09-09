8476985530824135181cdff2d09265a5fa64d66b
Rasik <vlad.fokin2004.vf@gmail.com>
2024-09-09 12:25:26 +0300

    Add mobile navigation to the landing page

--- src/components/landing/landing-header.tsx
+++ src/components/landing/landing-header.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import {header} from "@/config/landing.config";
 import {Button} from "@/components/ui/button";
+import {Sheet, SheetContent, SheetHeader, SheetTrigger,} from "@/components/ui/sheet"
+import {Ellipsis} from "lucide-react";
 
 function LandingHeader() {
     return (
@@ -10,8 +12,8 @@ function LandingHeader() {
                 <Image src={header.logoImagePath} alt='Cairos logo' width={200} height={43}/>
                 <span className="sr-only">Cairos</span>
             </Link>
-            <nav className="ml-auto flex gap-4 sm:gap-6 items-center">
-                { Object.entries(header.navigation).map(([key, value]) => (
+            <nav className="ml-auto gap-4 sm:gap-6 items-center hidden sm:flex">
+                {Object.entries(header.navigation).map(([key, value]) => (
                     <Link
                         key={key}
                         href={value.url}
@@ -20,13 +22,40 @@ function LandingHeader() {
                     >
                         {value.label}
                     </Link>
-                )) }
+                ))}
                 <Link href="/login" prefetch={false}>
                     <Button>
                         Увійти
                     </Button>
                 </Link>
             </nav>
+            <div className="sm:hidden flex w-full justify-end">
+                <Sheet>
+                    <SheetTrigger>
+                        <Ellipsis size={36}/>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            {Object.entries(header.navigation).map(([key, value]) => (
+                                <Link
+                                    key={key}
+                                    href={value.url}
+                                    prefetch={false}
+                                >
+                                    <Button variant="outline" className="w-full">
+                                        {value.label}
+                                    </Button>
+                                </Link>
+                            ))}
+                            <Link href="/login" prefetch={false}>
+                                <Button className="w-full">
+                                    Увійти
+                                </Button>
+                            </Link>
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
+            </div>
         </header>
     );
 }
